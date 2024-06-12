bfbdb2648613b4d9fe12d586622dd45cfd7bd6f5
Arjee <arjeenunez@gmail.com>
2024-06-12 18:44:26 +0800

    React states demo using the hangman game

---
+++ src/Game.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import Hangman from "./Hangman";
+import { randomWord } from "./words";
+import "./Game.css"
+
+class Game extends Component {
+    static defaultProps = {
+        keyboardLetters: Array.from({length: 26}, (el, i) => String.fromCharCode(97 + i))
+    }
+    constructor(props) {
+        super(props);
+        const org = randomWord().split("");
+        this.state = {
+            numWrong: 0,
+            word: [...org],
+            lettersGuessed: [],
+            remWord: [...org]
+        }
+        this.replay = this.replay.bind(this);
+        this.guessLetter = this.guessLetter.bind(this);
+    }
+    replay() {
+        const org = randomWord().split("");
+        this.setState(st => ({
+            numWrong: 0,
+            word: [...org],
+            lettersGuessed: [],
+            remWord: [...org]
+        }))
+    }
+    guessLetter(evt) {
+        const guessedLetter = evt.target.innerText;
+        const { word, lettersGuessed } = this.state;
+        const isGuessed = lettersGuessed.includes(guessedLetter);
+        const isCorrect = word.includes(guessedLetter);
+
+        if (isGuessed) return null;
+        if (isCorrect) {
+            this.setState(st => ({
+                numWrong: st.numWrong,
+                remWord: st.remWord.filter(el => el !== guessedLetter),
+                lettersGuessed: [...st.lettersGuessed, guessedLetter],
+                word: st.word
+            }))
+        } else {
+            this.setState(st => ({
+                numWrong: st.numWrong + 1,
+                remWord: st.remWord,
+                lettersGuessed: [...st.lettersGuessed, guessedLetter],
+                word: st.word
+            }))
+        }
+
+    }
+    render() {
+        const { keyboardLetters } = this.props;
+        const { numWrong, remWord, word, lettersGuessed } = this.state;
+        let msg;
+        if (numWrong > 5) msg = <h2>You lose! The word is "{word}".</h2>
+        if (remWord.length === 0 && numWrong < 6) msg = <h2>You win! Nice!</h2>
+
+        
+        console.log(this.state.remWord)
+        return (
+            <div className="Game">
+                <h1>Hangman Game!</h1>
+                <Hangman numWrong={this.state.numWrong} />
+                <div className="Game-word-container"> 
+                        {word.map((letter, i) => (
+                            <div className="Game-word-letter-container">
+                                <h1 key={ i } className={remWord.includes(letter) ? "Game-word-letter-hide" : "Game-word-letter"}>{ letter }</h1>
+                            </div>
+                        ))}
+                </div>
+                <div className="Game-button-keys-container">
+                    {keyboardLetters.map((letter, i) => (
+                        <button key={ i } onClick={this.guessLetter} className="Game-button-keys" disabled={lettersGuessed.includes(letter) || word.length === 0}>{ letter }</button>
+                    ))}
+                </div>
+                <div className="Game-message-container">
+                    { msg }
+                </div>
+                <button onClick={this.replay} className="Game-button-replay">New game?</button>
+            </div>
+        )
+    }
+}
+
+export default Game;
---
+++ src/Hangman.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Hangman.css"
+import img0 from "./images/0.jpg"
+import img1 from "./images/1.jpg"
+import img2 from "./images/2.jpg"
+import img3 from "./images/3.jpg"
+import img4 from "./images/4.jpg"
+import img5 from "./images/5.jpg"
+import img6 from "./images/6.jpg"
+
+class Hangman extends Component {
+    static defaultProps = {
+        numWrong: 1,
+        images: [img0, img1, img2, img3, img4, img5, img6]
+    }
+    render() {
+        const { images, numWrong } = this.props;
+        const msg = `${numWrong} wrong guesses!`
+        return (
+            <div className="Hangman-container">
+                <img className="Hangman" src={images[numWrong]} alt={ msg } />
+            </div>
+        )
+    }
+}
+
+export default Hangman;
